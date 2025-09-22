0f3c4ee9d2ab3f6bec49f32485638dd857f20942
CPPavithra <cppavithra05@gmail.com>
2025-09-23 00:06:55 +0530

    data for daily challenge; working

---
+++ app/src/main/code/poselandmarker/data/Challenge.kt
@@ -0,0 +1,8 @@
+package com.google.mediapipe.examples.poselandmarker.data
+
+data class Challenge(
+    val title: String,
+    val description: String,
+    val repGoal: Int,
+    val exerciseType: String // e.g., "Push-ups"
+)
