0ed62f1f6e3eb8b476f4e29a4a40c1fdc037c11e
CPPavithra <cppavithra05@gmail.com>
2025-09-26 00:47:17 +0530

    deleted deprecated files

--- app/src/main/code/poselandmarker/data/Challenge.kt
+++
@@ -1,8 +0,0 @@
-package com.google.mediapipe.examples.poselandmarker.data
-
-data class Challenge(
-    val title: String,
-    val description: String,
-    val repGoal: Int,
-    val exerciseType: String // e.g., "Push-ups"
-)
